fab5898bb5abe02416d8381941fc153ae31d59e0
JangSihoon <135976413+JangSihoon@users.noreply.github.com>
2023-06-15 19:18:56 +0900

    Create 9주차.py

---
+++ 9주차.py
@@ -0,0 +1,62 @@
+# a = 2
+# # if a 짝수라면?:
+# # 짝수는 2로 나누었을 때 나머지가 0
+# if 0 == a % 2:
+#     print("even")
+# else:
+#     print("odd")
+# if a == a/2:  # a == 0?
+#     pass
+
+
+#  |
+# / \
+# |-| if-else (a ,b 누가 크니?)
+# \ /
+#  |
+# / \
+# |-| if-else (a ,b 같니?)
+# \ /
+#  |
+
+
+# == 같다. = 초기화
+
+# end = "\n"
+
+
+def print(value: object, sep: str, end: str):
+    pass
+
+
+# end : 숫자가 들어갈 필요가 없어
+# 나 출력하고나서 마지막 어떤걸해줘
+
+# sep : 여러개 출력할때 뭐로 구분해줘
+
+# # int,float : 숫자
+# 숫자 -> 계산
+선생님 — 2023.05.25. 오후 9:19
+# 에러 대해서 ,,,
+
+# if 3 > 0
+#     print()
+# SyntaxError
+
+# var = 1
+# print(vaR)
+# NameError
+
+# print(1, sep=321)
+# print(3 + "string")
+# TypeError
+
+# int("string")
+# ValueError
+
+# 3/0
+# ZeroDivisionError
+
+# a = [0, 1, 2]
+# a[6]
+# IndexError
